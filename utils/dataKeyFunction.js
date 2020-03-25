f847e9d1624c9fef58fc5a8717fb525fda730048
stpark89 <stpark89@gmail.com>
2020-03-25 17:22:07 +0900

    검색

---
+++ utils/dataKeyFunction.js
@@ -0,0 +1,22 @@
+
+const dataKey = (keyName) => {
+
+    console.log("함수 호출 ---");
+    console.log(keyName);
+    console.log("함수 호출 ---");
+
+    return ;
+
+    switch (keyName) {
+
+        case '강좌명':
+
+            break;
+
+        case '':
+
+        default : 
+            console.log("없어여");
+    }
+
+}
